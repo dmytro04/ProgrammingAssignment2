ab80391bc4259b6f12ad9a8d47b000b3d35e74d1
dmytro04 <soroka.dmytro@lll.kpi.ua>
2023-01-22 13:30:52 +0200

    added makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## making cache matrix and saving it in y
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      y<<-solve(x) 
+      last_m<<-x
 }
 
 
-## Write a short comment describing this function
+## checking if inverse matrix is cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m<-NULL
+        a<-NULL
+        if(exists("m")&&exists("last_m")){
+        m<-y
+        a<-last_m
+        }
+        if(!is.null(m)&&x==a){
+              message("getting cached data")
+              return(m)
+        }
+        else{
+              m<-solve(x)
+        }
+        m
 }
+
